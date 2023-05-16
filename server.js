df7df6f7d1d51f3d96894edfa15079ebfd8f5dd1
Mxwsic <danielapacible2@gmail.com>
2023-05-15 20:24:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
-require('dotenv').config();
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const routes = require('./controllers/');
-const sequelize = require('./config/connection');
+const path = require('path'); 
+const express = require('express'); 
+const session = require('express-session'); 
+const exphbs = require('express-handlebars'); 
+const routes = require('./controllers'); 
+const helpers = require('./utils/helpers'); 
+const sequelize = require('./config/connection'); 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const helpers = require('./utils/helpers');
+
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001; 
 
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'shh its a secret',
-    cookie: {
-      maxAge: 900,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
 app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(routes);
 
-sequelize.sync({ force: true }).then(() => {
-  app.listen(PORT, () => console.log(`App listening at http://localhost:${PORT}`));
-});
+app.use(routes);
+ 
+sequelize.sync();
+
+  app.listen(PORT, () => console.log(`Now listening on port http://localhost:${PORT}`));
